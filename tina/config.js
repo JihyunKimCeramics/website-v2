430ee42714e72f77ee5a123bb429fe2d91f811d9
louiscutner <56656319+louiscutner@users.noreply.github.com>
2025-01-04 15:34:24 +0000

    added tina

---
+++ tina/config.js
@@ -0,0 +1,81 @@
+import { defineConfig } from "tinacms";
+
+// export const config = defineConfig({
+//   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+//   branch:
+//     process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
+//     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
+//     process.env.HEAD, // Netlify branch env
+//   token: process.env.TINA_TOKEN,
+//   media: {
+//     tina: {
+//       publicFolder: "public",
+//       mediaRoot: "uploads",
+//     },
+//   },
+//   build: {
+//     publicFolder: "public", // The public asset folder for your framework
+//     outputFolder: "admin", // within the public folder
+//   },
+//   schema: {
+//     collections: [exhibitions, about, home, work], // Include the index collection
+//   },
+// });
+
+// export default config;
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
