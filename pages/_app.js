7fc6a802cdd1816c94c8dde330bdff3eaf8efb4b
louiscutner <56656319+louiscutner@users.noreply.github.com>
2025-01-05 21:59:06 +0000

    fixed email zoom in issue

--- pages/_app.js
+++ pages/_app.js
@@ -365,6 +365,10 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Jihyun Kim Ceramic</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </Head>
 
       {/* Mobile Menu */}
