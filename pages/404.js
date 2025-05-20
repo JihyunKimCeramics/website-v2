02d44e53bc175e140c496a663d4dcd28bf7c1b7d
louiscutner <56656319+louiscutner@users.noreply.github.com>
2025-05-20 12:43:33 +0900

    fixed compiling issue

--- pages/404.js
+++ pages/404.js
@@ -1,40 +1,6 @@
-// import React from "react";
-// import NoPageMessage from "../components/NoPageMessage";
-// import Layout from "../components/Layout";
-// import { useTina } from "tinacms/dist/react";
-
-// export default function Custom404({ Component, pageProps }) {
-//   const tinaResult = useTina({
-//     query: pageProps.query,
-//     variables: pageProps.variables,
-//     data: pageProps.data,
-//   });
-
-//   const data = tinaResult.data?.data || pageProps.data?.data;
-
-//   return (
-//     <>
-//       {data ? (
-//         <Layout data={{ data }}>
-//           <NoPageMessage buttonColour="#F7EFE2" buttonHoverColour="#EDE2CB" />;
-//         </Layout>
-//       ) : (
-//         <Component {...pageProps} />
-//       )}
-//     </>
-//     // {data ? (
-//     //         <Layout data={{ data }}>
-//     //           <Component {...pageProps} />
-//     //         </Layout>
-//     //       ) : (
-//     //         <Component {...pageProps} />
-//     //       )}
-//   );
-// }
-
 import { tinaField, useTina } from "tinacms/dist/react";
 import { client } from "../tina/__generated__/client";
-import NoPageMessage from "../components/NoPageMessage";
+import NoPageMessage from "/components/NoPageMessage";
 
 export default function Custom404(props) {
   const { data } = useTina({
